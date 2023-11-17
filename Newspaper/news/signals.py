c54fecd3eb0c088e352c03caa5d02e677c911d11
NAPapanova <112985158+NAPapanova@users.noreply.github.com>
2023-11-17 11:34:58 +0300

    Add files via upload

---
+++ Newspaper/news/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save, m2m_changed
+from django.dispatch import receiver # импортируем нужный декоратор
+from django.core.mail import mail_user
+from .models import PostCategory
+import threading
+
+
+from basic.py import new_post_subscription
+import sys
+sys.path.insert(0, r'P:\University\pythonProject\Newspaper(2)\Newspaper\news\tasks')
+@receiver(m2m_changed, sender = PostCategory)
+# функция обработчик с параметрами под регистрацию сигнала
+def new_post_notify(sender, instance, created, **kwargs):
+    
+    if kwargs['action'] == 'post_add':
+        new_post_subscription(instance)
+    
+ 
+# коннектим наш сигнал к функции обработчику и указываем, к какой именно модели после сохранения привязать функцию
+post_save.connect(new_post_notify, sender=PostCategory)
+
+def notify_subscribers_next_week():
+  timer = threading.Timer(604800, new_post_notify) #таймер на 1 неделю
